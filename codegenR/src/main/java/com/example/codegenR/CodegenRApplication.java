fa0bbc1d550c72e89f8faea0d485552bb91afaa8
AqibMustafa <55453434+AqibMustafa@users.noreply.github.com>
2021-10-15 11:12:40 +0530

    initial commit

---
+++ codegenR/src/main/java/com/example/codegenR/CodegenRApplication.java
@@ -0,0 +1,13 @@
+package com.example.codegenR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodegenRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodegenRApplication.class, args);
+	}
+
+}
---
+++ codegenR/src/test/java/com/example/codegenR/CodegenRApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codegenR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodegenRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
